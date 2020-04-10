1206a0405ddbdd643d0b1d17d39e222016923419
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-04-10 18:45:03 -0500

    Add files via upload

---
+++ Lineal/src/lineal/Lineal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lineal;
+
+/**
+ *
+ * @author david
+ */
+public class Lineal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmLineal frmlinea = new FrmLineal();
+        frmlinea.setVisible(true);
+        frmlinea.setLocationRelativeTo(null);
+        frmlinea.setSize(400,300);
+        frmlinea.setTitle("Busqueda Lineal");
+    }
+    
+}
